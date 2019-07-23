2abb54b446a7e66690c7c57dad65c38bd41efc55
alexeispirit <alexeispirit@gmail.com>
2019-07-23 10:12:16 +0500

    Save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -1,38 +1,81 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Palette from './Pallete';
-import PaletteList from './PaletteList';
-import SingleColorPalette from './SingleColorPalette';
-import NewPaletteForm from './NewPaletteForm';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Palette from "./Pallete";
+import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
-  state = { palettes: seedColors }
+  constructor(props) {
+    super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
+  }
 
   findPalette = id => {
     return this.state.palettes.find(palette => palette.id === id);
-  }
+  };
 
   savePalette = newPalette => {
-    this.setState({ palettes: [...this.state.palettes, newPalette]})
-  }
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  };
+
+  syncLocalStorage = () => {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
+  };
 
   render() {
     return (
       <Switch>
-        <Route exact path='/palette/new' render={(routeProps) => <NewPaletteForm 
-                                                                    savePalette={this.savePalette} 
-                                                                    palettes={this.state.palettes}
-                                                                    {...routeProps} />} />
-        <Route exact path='/' render={(routeProps) => <PaletteList 
-                                                        palettes={this.state.palettes} 
-                                                        {...routeProps} />} />
-        <Route exact path='/palette/:id' render={(routeProps) => <Palette 
-                                                                    palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
-        <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => <SingleColorPalette 
-                                                                                    palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
-                                                                                    colorId={routeProps.match.params.colorId} />} />
+        <Route
+          exact
+          path="/palette/new"
+          render={routeProps => (
+            <NewPaletteForm
+              savePalette={this.savePalette}
+              palettes={this.state.palettes}
+              {...routeProps}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={routeProps => (
+            <PaletteList palettes={this.state.palettes} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={routeProps => (
+            <SingleColorPalette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+              colorId={routeProps.match.params.colorId}
+            />
+          )}
+        />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
