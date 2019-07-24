6cb3e10448fcc7c7fb60d49eed6a7f509211de7c
alexeispirit <alexeispirit@gmail.com>
2019-07-24 10:06:12 +0500

    Add basic route transitions

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -4,10 +4,6 @@ import styles from "./styles/MiniPaletteStyles";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 class MiniPalette extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   deletePalette = event => {
     event.stopPropagation();
     this.props.openDialog(this.props.id);
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,9 +8,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
-import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import styles from "./styles/PaletteFormNavStyles";
 
 import PaletteMetaForm from "./PaletteMetaForm";
@@ -34,7 +32,7 @@ class PaletteFormNav extends Component {
 
   render() {
     const { classes, open, palettes, handleSubmit } = this.props;
-    const { newPaletteName } = this.state;
+    // const { newPaletteName } = this.state;
     return (
       <div className={classes.root}>
         <CssBaseline />
